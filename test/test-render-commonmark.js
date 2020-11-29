36b7de64f510424e8a64c7428d54e8a681048c3a
neveisa <sbmohant@usc.edu>
2020-11-29 03:56:47 -0800

    katex working

--- test/test-render-commonmark.js
+++ test/test-render-commonmark.js
@@ -1,5 +1,5 @@
-var assert = require('assert')
-var texme = require('../texme.js')
+const assert = require('assert')
+const texme = require('../texme.js')
 
 describe('renderCommonMark', function () {
   before(function () {
@@ -8,8 +8,8 @@ describe('renderCommonMark', function () {
   })
 
   it('simple', function () {
-    var input = '*Foo* **Bar** `Baz`'
-    var expected = '<p><em>Foo</em> <strong>Bar</strong> <code>Baz</code></p>\n'
+    const input = '*Foo* **Bar** `Baz`'
+    const expected = '<p><em>Foo</em> <strong>Bar</strong> <code>Baz</code></p>\n'
     assert.deepStrictEqual(texme.renderCommonMark(input), expected)
   })
 })
